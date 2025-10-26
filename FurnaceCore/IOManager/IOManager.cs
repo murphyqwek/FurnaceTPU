cdbf7adcab4f39ae80e3da1fb480a68f43a22919
murphyqwek <qwekinaft@list.ru>
2025-10-26 19:33:26 +0300

    Create IOManager  with basic functionality
    IOManager handles messages from port and routing the data using filters
    
    Now IOManager can register new filter, handle data and process data

---
+++ FurnaceCore/IOManager/IOManager.cs
@@ -0,0 +1,37 @@
+﻿using FurnaceCore.Filters;
+using FurnaceCore.Port;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnaceCore.IOManager
+{
+    public class IOManager
+    {
+        private List<IFilter> _moduleFilters = new List<IFilter>();
+
+        public void RegisterFilter(IFilter filter)
+        {
+            _moduleFilters.Add(filter);
+        }
+
+        private void ProcessData(string data)
+        {
+            foreach (var filter in _moduleFilters)
+            {
+                if (filter.CanHandle(data))
+                {
+                    filter.Handle(data);
+                    break;
+                }
+            }
+        }
+
+        public void HandleData(string data)
+        {
+            ProcessData(data);
+        }
+    }
+}
