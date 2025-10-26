16228f15c608b81355d5e67a725b9cb253300ad6
murphyqwek <qwekinaft@list.ru>
2025-10-26 19:29:24 +0300

    Create IFilter interface
    It will be used for catching messages for certain module in routing messages in IOManager

---
+++ FurnaceCore/Filters/IFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnaceCore.Filters
+{
+    public interface IFilter
+    {
+        public bool CanHandle(string data);
+        public void Handle(string data);
+    }
+}
