4667860c99f6e6a9b6d638cbcb28541d1f0d201f
murphyqwek <qwekinaft@list.ru>
2025-10-26 19:25:50 +0300

    Add IPort interface
    This interface will be used in IOManager, so we can abstract port and create different types of ports (espacally test port)

---
+++ FurnaceCore/Port/IPort.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnaceCore.Port
+{
+    public interface IPort
+    {
+        public string Name { get; set; }
+        public void OpenPort();
+        public void ClosePort();
+        public bool IsOpen();
+        public void SendData(string data);
+    }
+}
