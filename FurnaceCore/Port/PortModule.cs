b438ff5991bdf50c44dd98b4d405bb6a898f4730
murphyqwek <qwekinaft@list.ru>
2025-10-26 19:26:11 +0300

    Create PortModule for working with SerialPort

---
+++ FurnaceCore/Port/PortModule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace FurnaceCore.Port
+{
+    public class PortModule(string name) : IPort
+    {
+        private string _name = name;
+        private SerialPort _serialPort = new SerialPort(name);
+
+        public string Name 
+        { 
+            get => _name;
+            set
+            {
+                if (_serialPort.IsOpen)
+                    throw new InvalidOperationException("Cannot change port name while the port is open.");
+                _name = value;
+                _serialPort.PortName = value;
+            }
+        }
+        public void OpenPort()
+        {
+            _serialPort.Open();
+        }
+
+        public void ClosePort()
+        {
+            _serialPort.Close();
+        }
+
+        public bool IsOpen()
+        {
+            return _serialPort.IsOpen;
+        }
+
+        public void SendData(string data)
+        {
+            _serialPort.WriteLine(data);
+        }
+
+        private void SerialPortDataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string receivedData = _serialPort.ReadLine();
+        }
+    }
+}
