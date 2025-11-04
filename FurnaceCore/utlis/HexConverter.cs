7075262afd9ce39f2c9507ac1806a3a0660a16d6
murphyqwek <qwekinaft@list.ru>
2025-11-04 18:09:14 +0300

    Add hex to byte converter

---
+++ FurnaceCore/utlis/HexConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnaceCore.utlis
+{
+    public static class HexConverter
+    {
+        public static bool TryHexStringToByteArray(string hex, out byte[]? result)
+        {
+            result = null;
+
+            if (string.IsNullOrEmpty(hex))
+                return false;
+
+            hex = hex.Replace("0x", "").Replace(" ", "").Replace("-", "");
+
+            if (hex.Length % 2 != 0)
+                return false;
+
+            if (!hex.All(c => "0123456789ABCDEFabcdef".Contains(c)))
+                return false;
+
+            try
+            {
+                result = Enumerable.Range(0, hex.Length / 2)
+                                  .Select(x => Convert.ToByte(hex.Substring(x * 2, 2), 16))
+                                  .ToArray();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
