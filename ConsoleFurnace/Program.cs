5a0ef8bbf257cccb6e60eb0e8f78def8fe24ab78
murphyqwek <qwekinaft@list.ru>
2025-11-18 20:56:19 +0300

    Add project for console tests

---
+++ ConsoleFurnace/Program.cs
@@ -0,0 +1,18 @@
+﻿using FurnaceCore.Model;
+using FurnaceCore.utlis;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleFurnace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double coeff = 327.058824;
+
+            double value = HexConverter.ConvertHexChannelDataToDouble("02 30".Replace(" ", ""));
+
+            Console.WriteLine(Math.Round(value * coeff, 3));
+        }
+    }
+}
