d588ad3f530f89c98f3a67acdf812dffa20c3c7b
murphyqwek <qwekinaft@list.ru>
2025-11-19 23:54:16 +0300

    Add test for temperature data get event

---
+++ FurnaceTest/TemperatureModuleEventTest.cs
@@ -0,0 +1,50 @@
+﻿using FurnaceCore.Filters;
+using FurnaceCore.IOManager;
+using FurnaceCore.Model;
+using FurnaceTest.Mock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnaceTest
+{
+    public class TemperatureModuleEventTest
+    {
+        [Theory]
+        [InlineData("01 16", 27.8)]
+        [InlineData("01 20", 28.8)]
+        [InlineData("01 30", 30.4)]
+        [InlineData("02 30", 56.0)]
+        public async Task GetTemperatureTest(string hexData, double temperature)
+        {
+            IOManager ioManager = new IOManager();
+            MockPort mockPort = new MockPort(ioManager);
+            TemperatureModule temperatureModule = new TemperatureModule(0x01, 0x01, ioManager);
+            ModbusAddressFilter modbusAddressFilter = new ModbusAddressFilter(0x01, 0x04, temperatureModule);
+
+            ioManager.RegisterModulePort(temperatureModule, mockPort);
+            ioManager.RegisterFilter(modbusAddressFilter);
+
+            bool eventTriggered = false;
+            double? eventValue = null;
+
+            temperatureModule.OnTemperatureGet += (value) =>
+            {
+                eventTriggered = true;
+                eventValue = value;
+            };
+
+            var task = temperatureModule.getTemperatureAsync();
+
+            mockPort.ReceiveData($"01 04 02 {hexData} 00 00");
+
+            double? result = await task;
+
+            Assert.True(eventTriggered);
+            Assert.Equal(result, temperature);
+            Assert.Equal(eventValue, temperature);
+        }
+    }
+}
