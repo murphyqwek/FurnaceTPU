7feed5ed5c765cd8a6fdf33f6d1f7126540354ac
murphyqwek <qwekinaft@list.ru>
2025-11-20 20:43:44 +0300

    Add BaseModbusFurnaceModule

--- FurnaceCore/Model/AddressChannelModbusFurnaceModule.cs
+++ FurnaceCore/Model/AddressChannelModbusFurnaceModule.cs
@@ -7,17 +7,15 @@ using System.Threading.Tasks;
 
 namespace FurnaceCore.Model
 {
-    public abstract class AddressChannelModbusFurnaceModule : IFurnaceModule
+    public abstract class AddressChannelModbusFurnaceModule : BaseModbusFurnaceModule
     {
         public readonly byte addressByte;
         public readonly byte channelByte;
-        protected IOManager.IOManager _ioManager;
 
-        public AddressChannelModbusFurnaceModule(byte addressByte, byte channelByte, IOManager.IOManager ioManager)
+        public AddressChannelModbusFurnaceModule(byte addressByte, byte channelByte, IOManager.IOManager ioManager): base(ioManager)
         {
             this.addressByte = addressByte;
             this.channelByte = channelByte;
-            this._ioManager = ioManager;
         }
 
         protected void InsertAddressesToCommand(ref byte[] command)
@@ -25,26 +23,5 @@ namespace FurnaceCore.Model
             command[0] = addressByte;
             command[3] = channelByte;
         }
-
-        protected byte[] GetCommandWithCRC(byte[] command)
-        {
-            int length = command.Length;
-
-            if(length < 2)
-            {
-                throw new ArgumentException("Length of command must be greater or equal then 2");
-            }
-
-            var crc = ModBusCRC.CalculateCRC(command);
-
-            var commandList = command.ToList();
-
-            commandList.Add(crc[0]);
-            commandList.Add(crc[1]);
-
-            return commandList.ToArray();
-        }
-
-        public abstract void HandleData(string data);
     }
 }
---
+++ FurnaceCore/Model/BaseModbusFurnaceModule.cs
@@ -0,0 +1,40 @@
+﻿using FurnaceCore.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnaceCore.Model
+{
+    public abstract class BaseModbusFurnaceModule
+    {
+        protected IOManager.IOManager _ioManager;
+
+        public BaseModbusFurnaceModule(IOManager.IOManager ioManager)
+        {
+            this._ioManager = ioManager;
+        }
+
+        protected byte[] GetCommandWithCRC(byte[] command)
+        {
+            int length = command.Length;
+
+            if (length < 2)
+            {
+                throw new ArgumentException("Length of command must be greater or equal then 2");
+            }
+
+            var crc = ModBusCRC.CalculateCRC(command);
+
+            var commandList = command.ToList();
+
+            commandList.Add(crc[0]);
+            commandList.Add(crc[1]);
+
+            return commandList.ToArray();
+        }
+
+        public abstract void HandleData(string data);
+    }
+}
