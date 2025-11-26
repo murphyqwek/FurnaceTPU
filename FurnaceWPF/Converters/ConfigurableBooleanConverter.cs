ecd5c0188cf1b6b9e1771f568c0dfc2a2595d03b
murphyqwek <qwekinaft@list.ru>
2025-11-26 21:12:28 +0300

    Add bool converter that is configurable

---
+++ FurnaceWPF/Converters/ConfigurableBooleanConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FurnaceWPF.Converters
+{
+    public class ConfigurableBooleanConverter : IValueConverter
+    {
+        public object TrueValue { get; set; }
+
+        public object FalseValue { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? TrueValue : FalseValue;
+            }
+
+            return FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && value.Equals(TrueValue);
+        }
+    }
+}
